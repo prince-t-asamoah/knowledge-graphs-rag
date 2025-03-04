1e3d204642a10d642e509b54fc91e8bc60e76a12
prince-t-asamoah <prince-t-asamoah@gmail.com>
2025-03-04 14:25:14 +0000

    pattern matching with multiple nodes

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ MATCH (n)
 RETURN  count(n) AS number_of_nodes
 """
 
-all_nodes = kg.query(all_nodes_cypher_query)
+# all_nodes = kg.query(all_nodes_cypher_query)
 # print(f'There are {all_nodes[0]["number_of_nodes"]} nodes in the graph')
 
 movie_nodes_cypher_query = """
@@ -24,7 +24,7 @@ MATCH (m:Movie)
 RETURN count(m) AS number_of_movies
 """
 
-all_movie_nodes = kg.query(movie_nodes_cypher_query)
+# all_movie_nodes = kg.query(movie_nodes_cypher_query)
 # print(f'There are {all_movie_nodes[0]["number_of_movies"]} movies in the graph')
 
 all_person_nodes_cypher_query = """
@@ -32,21 +32,21 @@ MATCH (p:Person)
 RETURN count(p) AS number_of_people
 """
 
-all_person_nodes = kg.query(all_person_nodes_cypher_query)
+# all_person_nodes = kg.query(all_person_nodes_cypher_query)
 # print(f'There are {all_person_nodes[0]["number_of_people"]} people in the graph')
 
 person_nodes_cypher_query = """
 MATCH(tom:Person {name:'Tom Hanks'})
 RETURN tom
 """
-single_person_node = kg.query(person_nodes_cypher_query)
+# single_person_node = kg.query(person_nodes_cypher_query)
 # print(single_person_node) 
 
 movie_nodes_cypher_query = """
 MATCH (cloudAtlas:Movie {title: 'Cloud Atlas'})
 RETURN cloudAtlas.released AS released_date, cloudAtlas.tagline AS tagline
 """
-single_movie_node = kg.query(movie_nodes_cypher_query)
+# single_movie_node = kg.query(movie_nodes_cypher_query)
 # print(single_movie_node)
 
 
@@ -57,5 +57,27 @@ WHERE nineties.released >= 1990 AND nineties.released < 2000
 RETURN nineties.title AS title
 """
 
-nineties_movie_nodes = kg.query(movie_realaese_date_cypher_query)
-print(nineties_movie_nodes)
+# nineties_movie_nodes = kg.query(movie_realaese_date_cypher_query)
+# print(nineties_movie_nodes)
+
+# Pattern matching with multiple nodes
+actors_acted_in_movie_cypher_query = """
+MATCH (actor:Person) - [:ACTED_IN] -> (movie:Movie)
+RETURN actor.name, movie.title LIMIT 10
+"""
+# actors_acted_in_movie_nodes = kg.query(actors_acted_in_movie_cypher_query)
+# print(actors_acted_in_movie_nodes)
+
+tom_hanks_acted_in_movie_cypher_query = """
+MATCH (tom:Person { title: 'Tom Hanks'}) - [:ACTED_IN] -> (tom_hanks_movies:Movie)
+RETURN tom.name, tom_hanks_movies.title
+"""
+# tom_hanks_acted_in_movie_nodes = kg.query(tom_hanks_acted_in_movie_cypher_query)
+# print(tom_hanks_acted_in_movie_nodes)
+
+tom_hanks_and_co_starred_actors_cypher_query = """
+    MATCH (tom:Person { name: 'Tom Hanks'}) - [:ACTED_IN] -> (m:Movie) <- [:ACTED_IN] - (co_actors: Person)
+    RETURN co_actors.name, m.title
+"""
+tom_hanks_and_co_starred_actors_nodes = kg.query(tom_hanks_and_co_starred_actors_cypher_query)
+print(tom_hanks_and_co_starred_actors_nodes)
